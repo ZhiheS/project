4870ed6c1661a1e7a659b3fbe6d23925da771be6
ZhiheS <shenzf@bc.edu>
2016-04-19 22:31:17 -0400

    Initial Commit

---
+++ project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Zhihe Shen on 4/19/16.
+//  Copyright (c) 2016 Zhihe Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
